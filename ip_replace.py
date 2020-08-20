9060f855629dd3afe0839b5e56404fa9172f9ebf
kevin <wei.liu@ifchange.com>
2020-08-20 16:52:06 +0800

    update 文档

--- ip_replace.py
+++ ip_replace.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python
 # coding:utf-8
 
-# 给你一个有效的 IPv4 地址 address，返回这个 IP 地址的无效化版本。
-#
-# 所谓无效化 IP 地址，其实就是用 "[.]" 代替了每个 "."。
+"""
+给你一个有效的 IPv4 地址 address，返回这个 IP 地址的无效化版本。
+
+所谓无效化 IP 地址，其实就是用 "[.]" 代替了每个 "."。
+"""
+
+
 def failureIPaddr(address: str) -> str:
     return address.replace(".", "[.]")
 
