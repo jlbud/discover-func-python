c3e90db0644cdf2d98b9ffd0db0a068d05b90ff4
kevin <wei.liu@ifchange.com>
2020-08-26 18:24:30 +0800

    add alog

---
+++ algo/game.py
@@ -0,0 +1,34 @@
+"""
+小A 和 小B 在玩猜数字。小B 每次从 1, 2, 3 中随机选择一个，小A 每次也从 1, 2, 3 中选择一个猜。他们一共进行三次这个游戏，请返回 小A 猜对了几次？
+
+输入的guess数组为 小A 每次的猜测，answer数组为 小B 每次的选择。guess和answer的长度都等于3。
+
+示例 1：
+输入：guess = [1,2,3], answer = [1,2,3]
+输出：3
+解释：小A 每次都猜对了。
+
+示例 2：
+输入：guess = [2,2,3], answer = [3,2,1]
+输出：1
+解释：小A 只猜对了第二次。
+"""
+from typing import List
+
+
+class Solution:
+    def game(self, guess: List[int], answer: List[int]) -> int:
+        count: int = 0
+        for i in range(0, len(answer)):
+            # 如果两个数相等
+            if answer[i] == guess[i]:
+                count = count + 1
+            # 不想等则跳过
+            else:
+                pass
+        return count
+
+
+s = Solution()
+ans = s.game([1, 2, 3, 4], [1, 2, 3, 10])
+print(ans)
