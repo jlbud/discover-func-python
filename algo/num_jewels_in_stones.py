ebe0b1ca9c5d7af270c28fdbd92a629208c131d2
kevin <wei.liu@ifchange.com>
2020-08-28 10:39:53 +0800

    石头中宝石的个数

---
+++ algo/num_jewels_in_stones.py
@@ -0,0 +1,26 @@
+"""
+给定字符串J 代表石头中宝石的类型，和字符串 S代表你拥有的石头。 S 中每个字符代表了一种你拥有的石头的类型，你想知道你拥有的石头中有多少是宝石。
+
+J 中的字母不重复，J 和 S中的所有字符都是字母。字母区分大小写，因此"a"和"A"是不同类型的石头。
+
+示例 1:
+输入: J = "aA", S = "aAAbbbb"
+输出: 3
+
+示例 2:
+输入: J = "z", S = "ZZ"
+输出: 0
+"""
+
+
+def num_jewels_in_stones(J: str, S: str) -> int:
+    count = 0
+    for x in S:
+        if x in J:
+            count += 1
+    return count
+
+
+n = num_jewels_in_stones("Aa", "aabbbb")
+# output: 2
+print(n)
