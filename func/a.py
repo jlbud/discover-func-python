99af20cf4485658aa6c72e81113a39e734c678b1
kevin <wei.liu@ifchange.com>
2020-08-28 14:23:20 +0800

    增加注释

--- func/a.py
+++ func/a.py
@@ -2,6 +2,10 @@ def abc():
     print("this is middle")
 
 
+# 被别的模块导入时也会执行
+print("running anyway anywhere")
+
+# 当模块abc()被导入时，下面代码不会执行
 if __name__ == '__main__':
     print("this is before")
     abc()
