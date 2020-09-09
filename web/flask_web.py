56f50104b62c9c59be461e890c1166c79b4e284d
kevin <wei.liu@ifchange.com>
2020-09-09 11:05:41 +0800

    add web&mysql

--- web/flask_web.py
+++ web/flask_web.py
@@ -1,5 +1,6 @@
 import json
 
+import pymysql
 from flask import Flask, request
 
 app = Flask(__name__)
@@ -21,6 +22,24 @@ def route_url():
         title = json_data.get("title")
         resp = {"response_code": 200, "title": title}
         return to_json(resp)
+
+
+@app.route('/mysql', methods=['GET', 'POST'])
+def route_get_mysql():
+    conn = pymysql.connect(host='115.28.56.***', port=3306, user='root', password='12345678', db='student',
+                           charset='utf8')
+    cur = conn.cursor()
+    cur.execute('select person from t1 order by id asc')
+    result = cur.fetchall()
+    cur.close()
+    conn.close()
+
+    rlt = {}
+    for i in result:
+        for j in i:
+            name = 'name%s' % j
+            rlt[name] = j
+    return to_json(rlt)
 
 
 def to_json(content):
