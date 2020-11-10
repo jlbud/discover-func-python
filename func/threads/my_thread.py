60c1af82701d716b0b30865747d2293efed2aaab
kevin <wei.liu@ifchange.com>
2020-11-10 17:24:05 +0800

    add my_thread

---
+++ func/threads/my_thread.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+class Thread(threading.Thread):
+    def __init__(self, func, args=()):
+        super(Thread, self).__init__()
+        self.func = func
+        self.args = args
+
+    def run(self):
+        self.result = self.func(*self.args)
+
+    def get_result(self):
+        try:
+            # 如果子线程不使用join方法，此处可能会报没有self.result的错误
+            return self.result
+        except Exception:
+            return None
